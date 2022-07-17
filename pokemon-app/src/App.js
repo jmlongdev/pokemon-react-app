24698a4c70431d45b43a89fc916f7d0772b19c27
Max <jmlong.dev@gmail.com>
2022-07-17 15:27:59 -0400

    added starting files

---
+++ pokemon-app/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import { useState } from "react";
+function App() {
+  return (
+    <div className="App">
+      <h1>Pokemon Stats</h1>
+    </div>
+  );
+}
+
+export default App;
